7e0932e97f29a08e164bd5c104467d6f733e77c0
Jaeseok Choi <cam_choi9@gwu.edu>
2024-05-05 16:29:01 -0500

    parity - primitive types - May 5th

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -3,9 +3,21 @@ from test_framework import generic_test
 
 def parity(x: int) -> int:
     # TODO - you fill in here.
+    result = 0
 
-    
-    return 0
+    # while x:
+    #     result = result ^ (x & 1)
+    #     x = x >> 1
+    #
+    # return result
+
+    count = 0
+
+    while x:
+        count += 1
+        x &= x-1
+
+    return count % 2
 
 
 if __name__ == '__main__':
