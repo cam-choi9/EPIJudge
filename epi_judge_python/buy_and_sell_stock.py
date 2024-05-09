c14fe72eb82a3f4ab475e0c42444ef4be4add9f3
Jaeseok Choi <cam_choi9@gwu.edu>
2024-05-08 23:27:05 -0500

    Buy & Sell Stock Once - Array - May 8th

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -5,7 +5,14 @@ from test_framework import generic_test
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
     # TODO - you fill in here.
-    return 0.0
+    min_price_so_far, max_profit = float('inf'), 0.0
+
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit_sell_today, max_profit)
+        min_price_so_far = min(min_price_so_far, price)
+
+    return max_profit
 
 
 if __name__ == '__main__':
